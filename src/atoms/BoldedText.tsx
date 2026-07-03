8851fea1ccf0e664e9ab4aa48d4fccc9be3c91be
ahmadmdabit <ahmadmdabit@gmail.com>
2026-07-03 07:33:10 +0300

    Updates

--- src/atoms/BoldedText.tsx
+++
@@ -1,9 +0,0 @@
-import { memo } from "react";
-
-interface BoldedTextProps {
-  text: string;
-}
-
-export const BoldedText: React.FC<BoldedTextProps> = memo(({ text }) => {
-  return <strong>{text}</strong>;
-});
--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-export function range(start: number, end: number): number[] {
-    const length = end - start + 1;
-    const result = new Array(length);
-    for (let i = 0; i < length; i++) {
-        result[i] = start + i;
-    }
-    return result;
-}
